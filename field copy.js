4c1ca6dc444e04e66c7ce4314cf93444dd6a9baa
Yanxin <42981100+yanxin0124@users.noreply.github.com>
2020-07-01 19:30:40 -0400

    current progress

---
+++ field copy.js
@@ -0,0 +1,200 @@
+//** Variables **********
+
+//Background
+var sky = "images/blueSky.png";
+
+// Player Animation
+var lastKey = "right";
+var playerIdle1 = "images/player/playerIdle1.png";
+var playerIdle2 = "images/player/playerIdle2.png";
+var playerIdle3 = "images/player/playerIdle3.png";
+var playerIdle;
+var playerRun1 = "images/player/playerRun1.png";
+var playerRun2 = "images/player/playerRun2.png";
+var playerRun3 = "images/player/playerRun3.png";
+var playerRun4 = "images/player/playerRun4.png";
+var playerRun;
+var jump = false;
+
+// Sounds
+var snd_windyPetals;
+
+// Environment
+var platformsGroup;
+var groundTop;
+var groundBot;
+var grass;
+var dirt;
+var petal;
+var petalsGroup;
+var pinkPetal;
+var grassBlade;
+var grassblade;
+var grassBlade1 = "images/environment/grassblade1.png";
+var grassBlade2 = "images/environment/grassblade2.png";
+var grassBlade3 = "images/environment/grassblade3.png";
+var grassBlade4 = "images/environment/grassblade4.png";
+var grassBlade5 = "images/environment/grassblade5.png";
+
+//** Preload *************
+function preload() {
+
+  skyBackground = loadImage(sky);
+
+  // Player Animations
+  playerIdle = loadAnimation(playerIdle1, playerIdle2, playerIdle1, playerIdle3, playerIdle1, playerIdle3);
+  playerRun = loadAnimation(playerRun1, playerRun2, playerRun3, playerRun2, playerRun1, playerRun4, playerRun3, playerRun2);
+
+  // Widgets
+  snd_windyPetals = loadSound("sounds/windyPetals.mp3");
+
+  // Environment
+  grass = loadImage("images/environment/grass.png");
+  dirt = loadImage("images/environment/dirt.png");
+  pinkPetal = loadImage("images/environment/petal.png");
+  grassBlade = loadAnimation(grassBlade1, grassBlade2, grassBlade3, grassBlade4, grassBlade5, grassBlade4, grassBlade3, grassBlade2);
+
+}
+
+//** Setup *************
+function setup() {
+
+	var canvas = createCanvas(window.innerWidth, window.innerHeight);
+  canvas.parent("field-div");
+  canvas.position(0, 0);
+  canvas.style("z-index", "-1");
+  var staticWindowWidth = window.innerWidth;
+
+  // Player Animations
+  playerIdle.frameDelay = 18;
+  playerRun.frameDelay = 13;
+
+  player = createSprite(staticWindowWidth/8, window.innerHeight-200, 100, 100);
+  player.addAnimation("idle", playerIdle);
+  player.addAnimation("run", playerRun);
+
+  // Platforms
+  var platformsAcross = Math.floor((staticWindowWidth / 100)) + 2;
+  platformsGroup = new Group();
+  for (i = 0; i < platformsAcross; ++i) {
+    groundTop = createSprite(i * 100, window.innerHeight-100, 100, 100);
+    groundTop.addImage(grass);
+    platformsGroup.add(groundTop);
+  }
+  for (i = 0; i < platformsAcross; ++i) {
+    groundBot = createSprite(i * 100, window.innerHeight, 100, 100);
+    groundBot.addImage(dirt);
+  }
+
+  // Environment
+  // Petals
+  let numOfPetals = 50;
+  petalsGroup = new Group();
+  for (i = 0; i < numOfPetals; ++i) {
+    let randomX = random(0, window.innerWidth * 1.2);
+    let randomY = random(0, window.innerHeight);
+    let petalX = random(-0.2, -1);
+    let petalY = random(1.5, 3.5);
+    let petalRotation = random(0, 4);
+    let scaleX = random(0.8, 1);
+    petal = createSprite(randomX, randomY, 14, 14);
+    petal.addImage(pinkPetal);
+    petal.rotation = random(0, 360);
+    petal.setVelocity(petalX, petalY);
+    petal.rotationSpeed = petalRotation;
+    petal.scale = scaleX;
+    petalsGroup.add(petal);
+  }
+  // Grassblades
+  grassBlade.frameDelay = 11;
+  let numOfGrassblades = 10;
+  for (i = 0; i < numOfGrassblades; ++i) {
+    let randomX = random(0, window.innerWidth);
+    grassblade = createSprite(randomX, window.innerHeight-162);
+    grassblade.addAnimation("grassBlade", grassBlade);
+  }
+
+} // function setup
+
+//** Draw ****************
+function draw() {
+
+  background(skyBackground);
+
+  // Player Movements
+  player.velocity.x = 0;
+  player.velocity.y += 0.85;
+  player.changeAnimation("idle");
+  if (lastKey == "right") {
+
+    player.mirrorX(1);
+  }
+  else if (lastKey == "left") {
+    player.mirrorX(-1);
+  }
+  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
+    lastKey = "right";
+    player.changeAnimation("run");
+    player.velocity.x = 6.5;
+  }
+  else if (keyIsDown(LEFT_ARROW) || keyIsDown(65)) {
+    lastKey = "left";
+    player.changeAnimation("run");
+    player.velocity.x = -6.5;
+  }
+  if (keyIsDown(UP_ARROW) || keyIsDown(87)) {
+    if (!jump) {
+      player.changeAnimation("idle");
+      player.velocity.y = -16;
+      jump = true;
+    }
+  }
+  for (var i = 0; i < platformsGroup.length; i++) {
+    if (platformsGroup[i].overlapPixel(player.position.x, player.position.y+50)) {
+      player.position.y -= 1;
+      player.velocity.y = 0;
+      jump = false;
+    }
+  }
+  if (player.position.x < 24) {
+    player.position.x = 24;
+  }
+
+  // Petals
+  for (i = 0; i < petalsGroup.length; ++i) {
+    if (petalsGroup[i].position.x < 0 || petalsGroup[i].position.y > window.innerHeight) {
+      petalsGroup[i].remove();
+      let randomX = random(0, window.innerWidth * 1.2);
+      let randomY = random(-100, 0);
+      let petalX = random(-0.75, -1.8);
+      let petalY = random(1.5, 3.5);
+      let petalRotation = random(0, 4);
+      let scaleX = random(0.8, 1);
+      petal = createSprite(randomX, randomY, 14, 14);
+      petal.addImage(pinkPetal);
+      petal.rotation = random(0, 360);
+      petal.setVelocity(petalX, petalY);
+      petal.rotationSpeed = petalRotation;
+      petal.scale = scaleX;
+      petalsGroup.add(petal);
+    }
+  }
+
+  drawSprites();
+
+} // function draw
+
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
+}
+
+// Widgets
+function toggleSound() {
+  var sound = document.getElementById("sound");
+  if (sound.checked) {
+    snd_windyPetals.loop();
+  }
+  else {
+    snd_windyPetals.pause();
+  }
+}
---
+++ mainMenu copy.js
@@ -0,0 +1,37 @@
+//** Variables **********
+//Background
+var sky = "images/blueSky.png";
+
+//** Preload *************
+function preload() {
+  skyBackground = loadImage(sky);
+}
+
+//** Setup *************
+function setup() {
+  var canvas = createCanvas(window.innerWidth, window.innerHeight);
+  canvas.parent("mainMenu-div");
+  canvas.position(0, 0);
+  canvas.style("z-index", "-1");
+} // function setup
+
+//** Draw ****************
+function draw() {
+  background(skyBackground);
+} // function draw
+
+function windowResized() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
+}
+
+function play() {
+  window.location = "field.html";
+}
+
+window.onload = function() {
+  window.setTimeout(fadeout, 3000);
+}
+
+function fadeout() {
+  document.getElementById('fadeout').style.opacity = '0';
+}
